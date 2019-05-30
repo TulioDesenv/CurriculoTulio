31fed6d3816895e7d83824e42ed49e896774393f
Marcos Tulio <tulio.desenv@gmail.com>
2019-05-30 16:39:31 -0400

    fix view path

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
+const path = require('path')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -8,8 +10,9 @@ app.get('/', (request, response) => {
     response.render('home')
 })
 
-const port = process.env.PORT || 3000
 
+
+app.set('views', path.join(__dirname, 'views'))
 app.listen(port, (err) => {
     if (err) {
         console.log('Não foi possivel iniciar o servidor do Curriculo Tulio')
